b85583a7c1ad91e94bfaf9169a2f76cada6f2afb
offlineric <offlineric@users.noreply.github.com>
2013-10-17 23:12:43 -0700

    a lot of small things that add up to a much more performant script

--- scrolling_class.user.js
+++ scrolling_class.user.js
@@ -16,65 +16,58 @@
 
 // ==/UserScript==
 
-
-(function($) {
-
-  /**
-   * Copyright 2012, Digital Fusion
-   * Licensed under the MIT license.
-   * http://teamdf.com/jquery-plugins/license/
-   *
-   * @author Sam Sehnert
-   * @desc A small plugin that checks whether elements are within
-   *     the user visible viewport of a web browser.
-   *     only accounts for vertical position, not horizontal.
-   */
-
-  $.fn.visible = function(partial) {
-    
-      var $t            = $(this),
-          $w            = $(window),
-          viewTop       = $w.scrollTop(),
-          viewBottom    = viewTop + $w.height(),
-          _top          = $t.offset().top,
-          _bottom       = _top + $t.height(),
-          compareTop    = partial === true ? _bottom : _top,
-          compareBottom = partial === true ? _top : _bottom;
-    
-    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
-
-  };
-    
-})(jQuery);
-
-
-var $modules = $('.postContainer, .summary, .pagelist');
-
-$(window).scroll(function(event) {
-  $modules.each(function(i, el) {
-    var $el = $modules.eq(i);
-    if ($el.visible(true)) {
-      $modules
-        .splice(i, 1);
-      $el
-        .addClass("come-in")
-
-    } 
-  });
-});
-var allMods = $(".postContainer, .summary, .pagelist");
-// Already visible modules
-allMods.each(function(i, el) {
-  var el = $(el);
-  if (el.visible(true)) {
-    el.addClass("come-in"); 
-  } 
-});
-
 $('link[rel=stylesheet]').remove();
 
 var win = $(window);
 var oldCount = 0;
+
+
+
+function inViewport (el, htmlClientHeight) {
+
+    var r;
+    if ( !el || 1 !== el.nodeType ) { return false; }
+
+    r = el.getBoundingClientRect();
+
+    return ( !!r 
+      && r.bottom >= 0 
+      && r.top <= htmlClientHeight 
+
+    );
+
+}
+
+
+var $modules = $('.postContainer, .summary, .pagelist');
+
+
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+
+var $modules = $('.postContainer, .summary, .pagelist');
+function seeModules () {
+    html = document.documentElement;
+var hch =  html.clientHeight;
+
+  $modules.each(function(i, el) {
+    var $el = $modules.eq(i);
+    if (inViewport(el, hch) == true) {
+      $modules
+        .splice(i, 1);
+      el.classList.add("come-in")
+
+    } 
+  });
+}
+
 
 
 document.addEventListener("ThreadUpdate", function(e){
@@ -121,7 +114,7 @@ function dragToKill()
   });
 }
      dragToKill();
-
+     seeModules();
 $(window).scroll(
     {
         previousTop: 0
@@ -133,6 +126,7 @@ $(window).scroll(
     } else {
 //down
         $("#header-bar").addClass("hide-it");
+                 if ($modules.length > 0) { requestAnimFrame(seeModules);}
     }
     this.previousTop = currentTop;
 });
